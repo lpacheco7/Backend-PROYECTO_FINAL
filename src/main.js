b7f30420535c08ec245ae2d6f52c1498ac8a25c4
Lorenzo Pacheco <lorenzopacheco02@gmail.com>
2026-07-02 00:22:22 -0300

    cors10

--- src/main.js
+++ src/main.js
@@ -17,10 +17,8 @@ connectMongoDB()
 const app = express();
 const PORT = ENVIRONMENT.PORT;
 
-console.log("https://frontend-proyecto-final-two.vercel.app");
-console.log("ENVIRONMENT.URLFRONTEND:", ENVIRONMENT.URL_FRONTEND);
 app.use(cors({
-    origin: ENVIRONMENT.URL_FRONTEND
+    origin: "https://frontend-proyecto-final-two.vercel.app"
 }));
 
 app.use(express.json());
